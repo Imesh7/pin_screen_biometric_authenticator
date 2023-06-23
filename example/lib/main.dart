41bc0cec06af64f65b9e539a4548b2c24c1ae0dd
2019imesh <2019imesh@gmail.com>
2023-06-23 15:21:04 +0530

    fix : add a dialog box to show the status in example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pin_input_biometric_authenticator/pin_input_biometric_authenticator.dart';
 
-
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
@@ -15,37 +14,46 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  ///String _platformVersion = 'Unknown';
-  //final _testPlugin = Test();
-  bool? status = false;
-
   @override
-  void initState() {
-    super.initState();
+  Widget build(BuildContext context) {
+    return MaterialApp(home: HomeScreen());
   }
+}
 
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
 
   final TextEditingController textEditingController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Center(
-          child: Column(
-            children: [
-              PinInptBiometricAuthenticator(
-                controller: textEditingController,
-                textFieldLength: 6,
-                authResultCallBack: (bool? result) {
-                  debugPrint("BIOMETRIC RESULT IS `$result`");
-                },
-              ),
-            ],
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Plugin example app'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            PinInptBiometricAuthenticator(
+              controller: textEditingController,
+              textFieldLength: 6,
+              authResultCallBack: (bool? result) {
+                debugPrint("BIOMETRIC RESULT IS `$result`");
+                showDialog(
+                    context: context,
+                    builder: (_) {
+                      return const SizedBox(
+                        height: 200,
+                        width: 200,
+                        child: AlertDialog(
+                          title: Center(child: Text("Bioemetric Sucess")),
+                          content: Text("You have Authenticated Successfully"),
+                        ),
+                      );
+                    });
+              },
+            ),
+          ],
         ),
       ),
     );
